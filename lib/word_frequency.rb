4f81946b6a5458245e0df87a8981c44972b090e0
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-01-10 17:05:02 -0800

    Rewrote code that passes first two specs

--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,12 +1,19 @@
+# class String
+# define_method(:word_frequency) do |other_words|
+#     answer_array = Array.new()
+#     counts = Hash.new(0)
+#     words = self.downcase().split()
+#     words.each() do |word|
+#       counts[word] += (1)
+#       answer_array = counts.fetch(word)
+#     end
+#     answer_array
+#   end
+# end
+
 class String
-define_method(:word_frequency) do |other_words|
-    answer_array = Array.new()
-    counts = Hash.new(0)
-    words = self.downcase().split()
-    words.each() do |word|
-      counts[word] += (1)
-      answer_array = counts.fetch(word)
-    end
-    answer_array
+  define_method(:word_frequency) do |argument_word|
+    words_input_array = downcase().split()
+    answer = words_input_array.count(argument_word)
   end
 end
