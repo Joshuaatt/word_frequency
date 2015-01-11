a4e692864e99f31dc34e8897245564a34c9daede
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-01-11 13:19:49 -0800

    Updated spec to better align with application requirements

--- spec/word_frequency_spec.rb
+++ spec/word_frequency_spec.rb
@@ -3,12 +3,12 @@ require('word_frequency')
 
 describe('String#word_frequency') do
 
-  it("takes a one letter input and returns 1") do
+  it("takes a one letter input, a one letter input to compare, and returns 1") do
     expect("a".word_frequency("a")).to(eq(1))
   end
 
   it("takes a two word input and returns 2") do
-    expect("two words".word_frequency("two words")).to(eq(2))
+    expect("two two".word_frequency("two")).to(eq(2))
   end
 
 
